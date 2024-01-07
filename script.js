90140d89931de80957fc3ea785481326837f6cf5
jonathanvitangeli <103973601+jonathanvitangeli@users.noreply.github.com>
2024-01-07 02:25:18 -0300

    first cimmit

---
+++ script.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper (".slider",{
+
+    sliderPerview:1,
+    spaceBetween:80,
+    grabCursor:true,
+    loop: true,
+breakpoints:{
+    991:{
+        sliderPerview:3
+    }
+}
+})
